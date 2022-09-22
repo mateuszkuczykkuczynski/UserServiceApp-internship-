1e481cb911438c7b17579fe359a41fbf571f6e5c
Kuczynski <mateusz.kuczynski@capgemini.com>
2022-09-22 21:55:22 +0200

    feat(redis): api app redis caching update

--- UserServiceApp/main.py
+++ UserServiceApp/main.py
@@ -1,9 +1,10 @@
+import json
 import uvicorn
 from fastapi import FastAPI, Query, status, HTTPException, Request, Response
-from fastapi_redis_cache import FastApiRedisCache, cache
+from fastapi_redis_cache import FastApiRedisCache
 from fastapi.encoders import jsonable_encoder
 from redis import Redis
-from sqlmodel import Session, SQLModel, select, or_
+from sqlmodel import Session, SQLModel, select
 from models import User, GetUser, PostUser
 from database import engine
 
@@ -15,15 +16,6 @@ REDIS_CONNECTION_STRING = 'redis://redis:6379'
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
-
-
-def delete_cache(id: int):
-    r = Redis('redis')
-    users = [user for user in r.scan_iter('*')]
-    for user in users:
-        decoded_value = r.get(user).decode('utf-8')
-        if f'\"id\": {id}' in decoded_value:
-            r.delete(user)
 
 
 @app.on_event("startup")
@@ -43,16 +35,53 @@ responses = {
 }
 
 
-def find_user_by_id_helper(ids):
-    return select(User).where(or_(User.id == user_id for user_id in ids))
+def find_user_by_nickname_helper(nicknames: str, session: Session):
+    result = session.exec(select(User).where(User.nickname == nicknames)).all()
+    index = session.exec(select(User.id).where(User.nickname == nicknames)).all()
+    return result, index
 
 
-def find_user_by_nickname_helper(nicknames):
-    return select(User).where((User.nickname == nicknames))
+def find_user_by_email_helper(emails: str, session: Session):
+    result = session.exec(select(User).where(User.email == emails)).all()
+    index = session.exec(select(User.id).where(User.email == emails)).all()
+    return result, index
 
 
-def find_user_by_email_helper(emails):
-    return select(User).where((User.email == emails))
+def find_user_by_id_helper(index_list: list[int], session: Session):
+    results_list = []
+    for index in index_list:
+        result = session.get(User, index)
+        if result:
+            results_list.append(session.get(User, index))
+    return results_list
+
+
+def find_all_users_helper(session: Session):
+    users = select(User)
+    result = session.exec(users).all()
+    index = session.exec(select(User.id)).all()
+    return result, index
+
+
+def create_cache_user_key(id, user_data):
+    try:
+        FastApiRedisCache().add_to_cache(key=f"user_cache:main.create_key_user_id={id}", value=user_data, expire=100)
+    except AttributeError as error:
+        print(error)
+
+
+def get_user_info_from_cache(id):
+    r = Redis('redis')
+    user_info = r.get(f"user_cache:main.get_user_key_id={id}")
+    if user_info:
+        user_info = json.loads(user_info)
+        return user_info
+
+
+def delete_caches(index):
+    r = Redis('redis')
+    keys = r.keys(f'*{index}*')
+    r.delete(*keys)
 
 
 @app.post("/v2/users", responses=responses, response_model=GetUser, status_code=status.HTTP_200_OK)
@@ -62,20 +91,26 @@ async def add_a_new_user(user: PostUser):
         session.add(db_user)
         session.commit()
         session.refresh(db_user)
-        delete_cache(db_user.id)
-        return db_user
+    try:
+        delete_caches(db_user.id)
+    except Exception as error:
+        print(error)
+    return db_user
 
 
 @app.get("/v2/users/{id}", responses=responses, response_model=GetUser, status_code=status.HTTP_200_OK)
-@cache(expire=60)
 async def get_user_info(response: Response, id):
     with Session(engine) as session:
-        user = session.get(User, id)
-        if not user:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                                detail="User Not Found")
-        encoded_user = jsonable_encoder(user)
-        return encoded_user
+        if get_user_info_from_cache(id):
+            encoded_user = get_user_info_from_cache(id)
+        else:
+            user = session.get(User, id)
+            encoded_user = jsonable_encoder(user)
+            create_cache_user_key(id, encoded_user)
+    if not encoded_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="User Not Found")
+    return encoded_user
 
 
 @app.put("/v2/users/{id}", responses=responses, response_model=GetUser, status_code=status.HTTP_200_OK)
@@ -91,8 +126,11 @@ async def update_user_info(id, user: PostUser):
         session.add(db_user)
         session.commit()
         session.refresh(db_user)
-        delete_cache(db_user.id)
-        return db_user
+    try:
+        delete_caches(id)
+    except Exception as error:
+        print(error)
+    return db_user
 
 
 @app.delete("/v2/users/{id}", responses=responses, status_code=200)
@@ -104,35 +142,41 @@ async def delete_user(id):
                                 detail="User Not Found")
         session.delete(user)
         session.commit()
-        delete_cache(user.id)
+    try:
+        delete_caches(id)
+    except Exception as error:
+        print(error)
 
 
 @app.get("/v2/users", responses=responses, status_code=200)
-@cache(expire=60)
 async def search_and_filter_users(response: Response, ids: list[int] = Query(default=None), emails: str = Query(default=None),
                                   nicknames: str = Query(default=None)) -> list:
     with Session(engine) as session:
         query_list = [ids, emails, nicknames]
         sorted_query_list = [i for i in query_list if i is not None]
         if len(sorted_query_list) == 0:
-            results = select(User)
+            result, index = find_all_users_helper(session)
         elif len(sorted_query_list) == 1:
             if emails:
-                results = find_user_by_email_helper(emails)
+                result, index = find_user_by_email_helper(emails, session)
             if nicknames:
-                results = find_user_by_nickname_helper(nicknames)
+                result, index = find_user_by_nickname_helper(nicknames, session)
             if ids:
-                results = find_user_by_id_helper(ids)
+                result = find_user_by_id_helper(ids, session)
+                index = ids
         elif len(sorted_query_list) > 1:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                                 detail="Request cannot be processed because parameters are mutually exclusive.")
-        users = session.exec(results).all()
-        encoded_user = jsonable_encoder(users)
-        if not users:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                                detail="User Not Found")
-        else:
-            return encoded_user
+    if get_user_info_from_cache(index):
+        decoded_results = get_user_info_from_cache(index)
+    else:
+        decoded_results = jsonable_encoder(result)
+        create_cache_user_key(index, decoded_results)
+    if not decoded_results:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="User Not Found")
+    else:
+        return decoded_results
 
 
 if __name__ == "__main__":
